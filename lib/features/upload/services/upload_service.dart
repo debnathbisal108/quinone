a4b4050eb25e89cc693a9a69a42df43ec3c726a4
debnathbisal108 <debnathbisal108@gmail.com>
2026-07-28 23:02:44 +0530

    Update upload_service.dart

--- lib/features/upload/services/upload_service.dart
+++ lib/features/upload/services/upload_service.dart
@@ -54,7 +54,7 @@ class UploadService {
       if (profile != null && profile.isNotEmpty) {
         formData.fields.add(
           MapEntry(
-            'profile',
+            'user_profile',
             jsonEncode(profile),
           ),
         );
