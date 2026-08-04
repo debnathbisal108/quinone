2605ca5b404b95b90302741f008fa977444522b4
debnathbisal108 <debnathbisal108@gmail.com>
2026-08-04 23:12:48 +0530

    Update profile_screen.dart

--- lib/features/onboarding/presentation/screens/profile_screen.dart
+++ lib/features/onboarding/presentation/screens/profile_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../providers/profile_provider.dart';
+import '../../profile/providers/profile_provider.dart';
 
 class ProfileScreen extends ConsumerStatefulWidget {
   const ProfileScreen({
